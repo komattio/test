cbac6ce9af902eebd38c38cda9820b07c93f672c
komattio <y.komatsu9@gmail.com>
2016-06-14 07:00:07 +0900

    add

---
+++ countdown.rb
@@ -0,0 +1,6 @@
+count = STDIN.gets
+count = count.to_i
+for n in 1..count
+    count -= 1
+    puts count
+end
---
+++ d035.rb
@@ -0,0 +1,5 @@
+hikisu = STDIN.gets
+str = hikisu.split(" ")
+
+puts str[0]
+puts str[1]
