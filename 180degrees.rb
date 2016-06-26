448d6bbe94953f527202616873d580188aff5753
komattio <y.komatsu9@gmail.com>
2016-06-26 09:36:21 +0900

    add

---
+++ 180degrees.rb
@@ -0,0 +1,12 @@
+line = Array.new
+
+line[0] = gets.chomp
+line[1] = gets.chomp
+line[2] = gets.chomp
+line[3] = gets.chomp
+
+puts
+puts line[3].reverse
+puts line[2].reverse
+puts line[1].reverse
+puts line[0].reverse
