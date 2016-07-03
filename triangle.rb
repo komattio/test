2be5d783e50d63f210f6fb64da2e82fee998ccd8
komattio <y.komatsu9@gmail.com>
2016-07-03 09:31:38 +0900

    fix calc loop

--- triangle.rb
+++ triangle.rb
@@ -2,12 +2,16 @@ n = gets.to_i
 a = gets.chomp.split(' ')
 ans = 0
 for i in 0..n-1
-    for j in i..n-1
-        for k in j..n-1
+    for j in i+1..n-1
+        for k in j+1..n-1
             len = a[i].to_i + a[j].to_i + a[k].to_i
+            p "len: " + len.to_s
             maa = [a[i].to_i, a[j].to_i, a[k].to_i]
+            p "maa: " + maa.to_s
             ma = maa.max
+            p "ma: " + ma.to_s
             rest = len - ma
+            p "rest: " + rest.to_s
             if ma < rest
                 if ma < len
                     ans = len
@@ -15,6 +19,8 @@ for i in 0..n-1
                     ans = ma
                 end
             end
+            p "ans: " + ans.to_s
+            p "-----------------"
         end
     end
 end
