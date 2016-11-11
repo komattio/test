2e44e3ae783ff0bcdc6f608d51989054b47fdb3d
komattio <y.komatsu9@gmail.com>
2016-11-12 00:53:26 +0900

    add

--- date-dir.sh
+++ date-dir.sh
@@ -3,3 +3,7 @@
 mkdir `date '+%Y%m%d'`
 touch `date '+%Y%m%d'`/`date '+%Y%m%d'`.txt
 echo `date '+%Y%m%d'` > `date '+%Y%m%d'`/`date '+%Y%m%d'`.txt
+
+git add .
+git commit -m "add"
+git push origin master 
