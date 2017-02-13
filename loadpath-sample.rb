4dd7ac35eae88f08699c8810c5064f38d7021b68
komattio <y.komatsu9@gmail.com>
2017-02-13 18:35:45 +0900

    add

---
+++ loadpath-sample.rb
@@ -0,0 +1,4 @@
+puts "__FILE__: #{__FILE__}"
+puts "File.dirname(__FILE__): #{File.dirname(__FILE__)}"
+puts "File.join(File.dirname(__FILE__), \"join\"): #{File.join(File.dirname(__FILE__), "join")}"
+
