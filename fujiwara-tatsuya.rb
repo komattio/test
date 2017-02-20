27a09ce7b7365a0fe465d17a29e99faa39b02a78
komattio <y.komatsu9@gmail.com>
2017-02-21 08:19:28 +0900

    add

--- fujiwara-tatsuya.rb
+++ fujiwara-tatsuya.rb
@@ -1,7 +1,10 @@
 print "メ゛ッ゛セ゛ー゛ジ゛を゛入゛力゛し゛て゛く゛だ゛さ゛い゛い゛い゛い゛い゛い゛い゛い゛：゛"
 inputmsg = gets.chomp
 outputmsg = "";
-inputmsg.each_char { |a| outputmsg = outputmsg + a + "゛" }
 
+# 藤原竜也風にする
+inputmsg.each_char { |a| outputmsg = outputmsg + a + "゛" }
 puts outputmsg
 
+# 藤原竜也風をもとに戻す
+puts outputmsg.delete("゛")
