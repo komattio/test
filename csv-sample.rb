739bd499d85022972940dad1ea92153411253282
komattio <y.komatsu9@gmail.com>
2017-03-18 11:41:52 +0900

    CSVの操作サンプル

--- csv-sample.rb
+++ csv-sample.rb
@@ -1,22 +1,29 @@
 begin
+  sales = []
+  engineers = []
   File.open('./members.csv')do |file|
-    puts file
-    puts "入力ファイルの内容"
+    puts "## 入力ファイルの内容"
     file.each_line do |line|
       puts line
     end
   end
 
   File.open('./members.csv')do |file|
-    puts file
-    puts "営業部の人だけ表示"
+    puts "## 営業部と技術部で分けて表示"
     file.each_line do |line|
       post = line.split(',')
       if(post[1] == "営業部")
-        puts line
+        sales.push(line)
+      elsif(post[1] == "技術部")
+        engineers.push(line)
       end
     end
   end
+
+  puts "営業部"
+  sales.each{|line| puts line}
+  puts "技術部"
+  engineers.each{|line| puts line}
 
 rescue SystemCallError => e
   puts %Q(class=[#{e.class}] message=[#{e.message}])
